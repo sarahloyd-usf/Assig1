250bd4cfad48a95f9eda320c0938c93a91032f78
sarahloyd-usf <sarahloyd@usf.edu>
2020-09-20 20:51:21 -0400

    Final

--- Assig1/Program.cs
+++ Assig1/Program.cs
@@ -43,18 +43,18 @@ namespace Assignment1_Fall20
             try
             {
                 //QUESTION 1
-                int a; // variable for columns?
-                int b; // variable for rows?
+                int a; // variable for rows
+                int b; // variable for columns
                 int c = x; //number of rows
 
-                for (a = 1; a <= x; a++) //columns, for loop continues until
+                for (a = 1; a <= x; a++) //loop through all the rows
                 {
-                    for (b = 1; b <= x; b++) //rows, for loop continues until
-                    {
+                    for (b = 1; b <= x; b++) //loop through the columns
+                    {// write spaces and stars
                         if (b >= c)
-                            Console.Write("* "); //star written if current row is greater or equal to input
+                            Console.Write("* "); 
                         else
-                            Console.Write(" "); //no star written if current row is less than input
+                            Console.Write(" "); 
                     }
                     c--;
                     Console.Write("\n"); //next line
@@ -71,23 +71,26 @@ namespace Assignment1_Fall20
             try
             {
                 //QUESTION 2
-                int a; //variable for
+                int a; //variable for the odd numbers
                 int sum = 0; //tracking the sum
 
                 Console.Write("The odd numbers are : ");
 
-                for (a = 1; a <= n; a+=2) //for loop starts at 1 and continues every 2 numbers
+                for (a = 1; a <= n; a+=2) //loop n times, increment by 2 each time for odd numbers
                 {
                     Console.Write(a);
 
+                    //keep a running sum of each number
                     sum = sum + a;
 
+                    // at the end, write a line feed, otherwise, write a comma
                     if (a < n)
-                        Console.Write(", "); //commas if...?
+                        Console.Write(", "); 
                     else
-                        Console.Write("\n"); //otherwise next line
+                        Console.Write("\n"); 
                 }
 
+                //display the sum
                 Console.WriteLine("The sum is : " + sum);
             }
             catch
@@ -102,21 +105,21 @@ namespace Assignment1_Fall20
             {
                 //QUESTION 3
                 bool result; //true or false
-                int a; //variable for??
+                int a; //variable for array loop
                 int b = 0; //1=inc 2=dec tracks whether monotone increasing or decreasing
 
-                result = true; //initially set to true
+                result = true; //initially set to true for the case of no change
 
-                for (a = 0; a <= n.Length; a++) //for loop continues as long as...?
+                for (a = 0; a <= n.Length; a++) //loop through each array value
                 {
-                    if (a == 0)
+                    if (a == 0) //first time through establish if it is inc or dec using b to store it
                     {
                         if (n[a + 1] > n[a])
                             b = 1;
                         else if (n[a + 1] < n[a])
                             b = 2;
                     }
-                    else
+                    else // for every other loop, check if inc or dec and compare it to b to see if it stays inc or dec, if it flips, then set to false and stop
                     {
                         if ((n[a + 1] > n[a]) & (b == 2))
                         {
@@ -144,18 +147,18 @@ namespace Assignment1_Fall20
             {
                 // QUESTION 4
                 int numPairs = 0; //number of pairs, initialized to 0
-                int a; //vsriable for??
-                int searchResult; //
+                int a; //for looping thorugh dedup array
+                int searchResult; //result returned from the binarySearch function
                 int[] deDup; //array without duplicates
 
-                deDup = J.Distinct().ToArray();
-                Array.Sort(deDup); //remove duplicates and sort
+                deDup = J.Distinct().ToArray(); //remove duplicate values from the array
+                Array.Sort(deDup); //sort the array
 
-                for (a = 0; a < deDup.Length - 1; a++) //for loop continues as long as...?
+                for (a = 0; a < deDup.Length - 1; a++) //loop through the array
                 {
-                    searchResult = binarySearch(deDup, a + 1, deDup.Length - 1, deDup[a] + k); //binary search to find...?
+                    searchResult = binarySearch(deDup, a + 1, deDup.Length - 1, deDup[a] + k); //search the rest of the array for this array value + k
 
-                    if (searchResult != -1) numPairs++;
+                    if (searchResult != -1) numPairs++; // if a match was found, increment the numPairs
                 }
                 return numPairs;
             }
@@ -195,13 +198,13 @@ namespace Assignment1_Fall20
                 char[] keyboardArray = keyboard.ToCharArray(); //convert string to array
 
 
-                foreach (char c in word) //set c variable, characters in word
+                foreach (char c in word) //loop thourgh each character in word
                 {
 
                     for (i = 0; i < keyboardArray.Length - 1; i++) //for loop continues as long as current position is within keyboard length
                     {
 
-                        if (keyboardArray[i] == c) //if the current position in the array is equal to the character, increase the time by the number of the current position
+                        if (keyboardArray[i] == c) //if the current position in the array is equal to the character then we found it, so increase the time by the number of the current position
                         {
                             time = time + i;
                         }
@@ -225,15 +228,18 @@ namespace Assignment1_Fall20
                 int a = str1.Length;
                 int b = str2.Length;
 
-                //special case
+                //This uses the known algorithm for minimum edit distance
+
+                //if str1 is blank, then the number of edits equals length of str2
                 if (a == 0)
                     return b;
 
-                //special case
+                //if str2 is blank, then the number of edits equals length of str1
                 if (b == 0)
                     return a;
 
-                //compare end values
+                //compare last values in strings
+                // if they're the same, just chop them off and recursively call the function
                 if (str1[a - 1] == str2[b - 1])
                 {
                     str1 = str1.Remove(a - 1);
@@ -242,10 +248,11 @@ namespace Assignment1_Fall20
                     return StringEdit(str1, str2);
                 }
 
+                //otherwise, the min of the three surrounding elements is the answer
                 return 1 + new int[] { StringEdit(str1, str2.Remove(b - 1)), // Insert 
                               StringEdit(str1.Remove(a - 1), str2), // Remove 
                               StringEdit(str1.Remove(a - 1), str2.Remove(b - 1)) // Replace 
-                               }.Min(); //minimum value taken from array
+                               }.Min(); 
             }
             catch
             {
